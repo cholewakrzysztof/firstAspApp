e23d49f63a0e8905ae301d54549bea1516064647
cholewakrzysztof <cholewa.krzysztof24@gmail.com>
2023-04-06 19:17:14 +0200

    Add class Car and page CarList for future work with database and routing training

---
+++ Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesPizza{
+    public class Car
+    {
+        [Required]
+        public int id_auto;
+        public string marka;
+        public string kolor;
+        public string nr_rej;
+        public int przebieg;
+        public int cena_pln;
+        public int cena_eu;
+        public int rok_prod;
+        public double pojemnnosc;
+        public Paliwo paliwo;
+    }
+    public enum Paliwo{benzyna,diesel}
+}
